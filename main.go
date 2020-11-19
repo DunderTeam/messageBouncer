58763a580a83dc2de00c5ced7d3e4efd23bdc34e
Infroz <infroz98@gmail.com>
2020-11-19 12:15:36 +0100

    Simple message bouncer

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/patrickmn/go-cache"
+	"log"
+	"net/http"
+	"time"
+)
+
+var Cache = cache.New(5*time.Minute, 5*time.Minute)
+
+func main() {
+
+	http.HandleFunc("/message", HandleMessage)
+
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal("Server not running", err)
+		return
+	}
+}
