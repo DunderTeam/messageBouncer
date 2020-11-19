f97746d4216a8c51b8002981ba5561fe2936d012
Infroz <infroz98@gmail.com>
2020-11-19 13:29:34 +0100

    Go Mod created

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 
 	http.HandleFunc("/message", HandleMessage)
 
-	err := http.ListenAndServe("localhost:8080", nil)
+	err := http.ListenAndServe("localhost:80", nil)
 	if err != nil {
 		log.Fatal("Server not running", err)
 		return
